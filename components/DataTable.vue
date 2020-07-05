5526d62cc54c94afcb44cda6e78372e45eaae070
Charles Ryan <charles.ryan@bvaccel.com>
2020-07-05 11:21:32 -0700

    rendering variations in absolute numbers

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -26,13 +26,24 @@
       class="elevation-1"
       :loading="loading"
     )
+    br
+    hr
+    br
+    v-data-table(
+      :headers="impactHeaders"
+      :items="impactData"
+      multi-sort
+      item-key="activity"
+      class="elevation-1"
+    )
 
 </template>
 
 <script>
 import Vuetify from 'vuetify'
 
-import userData from '../assets/staticData'
+import staticData from '../assets/staticData'
+import habitData from '../assets/habitData'
 
 export default {
   name: 'DataTable',
@@ -61,19 +72,140 @@ export default {
         },
         { text: 'REM Sleep Total', value: 'remDuration', show: false },
         { text: 'Deep Sleep Total', value: 'swsDuration', show: false }
-      ]
+      ],
+      rawImpactHeaders: [
+        { text: 'Activity', value: 'activity', show: true },
+        { text: 'HRV', value: 'hrv', show: true },
+        { text: 'Resting Heart Rate', value: 'rhr', show: true },
+        { text: 'Recovery Score', value: 'recovery', show: true },
+        { text: 'Sleep Score', value: 'sleep', show: true },
+        { text: 'Strain Score', value: 'strain', show: true },
+        { text: 'Respiratory Rate', value: 'respiratoryRate', show: true },
+        {
+          text: 'Light Sleep Total',
+          value: 'lightSleepDuration',
+          show: true
+        },
+        { text: 'REM Sleep Total', value: 'remDuration', show: true },
+        { text: 'Deep Sleep Total', value: 'swsDuration', show: true }
+      ],
+      tableData: [],
+      tableDataAvgs: {},
+      impactData: [],
+      habitData: habitData
     }
   },
   mounted() {
-    console.log(this.userData)
+    // console.log('userData', this.userData)
+    console.log('habitData', this.habitData)
     this.loading = false
+    this.parseTableData()
+    console.log('tableData', this.tableData)
+    this.getTableDataAvgs()
+    this.getImpactData()
   },
   computed: {
     headers() {
       return this.rawHeaders.filter((header) => header.show)
     },
-    tableData() {
-      return this.userData.map((item) => {
+    impactHeaders() {
+      return this.rawImpactHeaders.filter((header) => header.show)
+    }
+  },
+  methods: {
+    getImpactData() {
+      const impactSums = {}
+
+      this.habitData.forEach((el) => {
+        const date = el.Date
+        // get corresponding day's raw data
+        const dayData = this.tableData.find((day) => day.date === date)
+        if (dayData) {
+          Object.keys(el).forEach((activity) => {
+            // if the activity took place, add the variation to the activity's variation total and increment count
+            if (el[activity] !== 0) {
+              if (impactSums.hasOwnProperty(activity)) {
+                // increment
+                Object.keys(dayData).forEach((metric) => {
+                  if (dayData[metric] !== 'no data') {
+                    impactSums[activity][metric]['total'] += dayData[metric]
+                    impactSums[activity][metric]['count']++
+                  }
+                })
+              } else {
+                // initialize
+                Object.keys(dayData).forEach((metric) => {
+                  if (dayData[metric] !== 'no data') {
+                    if (!impactSums.hasOwnProperty(activity)) {
+                      impactSums[activity] = {}
+                    }
+
+                    impactSums[activity][metric] = {
+                      total: dayData[metric],
+                      count: 1
+                    }
+                  }
+                })
+              }
+            }
+          })
+        }
+      })
+
+      // calculate averages for each activity
+      Object.keys(impactSums).forEach((key1) => {
+        Object.keys(impactSums[key1]).forEach((key2) => {
+          impactSums[key1][key2]['average'] = Math.round(
+            impactSums[key1][key2]['total'] / impactSums[key1][key2]['count']
+          )
+        })
+      })
+
+      // compare activity averages to overall averages
+      Object.keys(impactSums).forEach((activity) => {
+        if (activity !== 'Date' && activity !== 'FIELD21') {
+          const impactItem = {
+            activity: activity
+          }
+          Object.keys(impactSums[activity]).map((metric) => {
+            if (this.tableDataAvgs[metric]) {
+              impactItem[metric] =
+                impactSums[activity][metric]['average'] -
+                this.tableDataAvgs[metric]['average']
+            }
+          })
+
+          this.impactData.push(impactItem)
+        }
+      })
+    },
+    getTableDataAvgs() {
+      this.tableData.forEach((el) => {
+        Object.keys(el).forEach((key) => {
+          if (el[key] !== 'no data' && key !== 'date') {
+            if (this.tableDataAvgs.hasOwnProperty(key)) {
+              this.tableDataAvgs[key]['total'] += el[key]
+              this.tableDataAvgs[key]['count']++
+            } else {
+              this.tableDataAvgs[key] = {
+                total: el[key],
+                count: 1
+              }
+            }
+          }
+        })
+      })
+
+      Object.keys(this.tableDataAvgs).forEach((key) => {
+        this.tableDataAvgs[key]['average'] = Math.round(
+          this.tableDataAvgs[key]['total'] / this.tableDataAvgs[key]['count']
+        )
+      })
+
+      console.log('sums', this.tableDataAvgs)
+    },
+    parseTableData() {
+      this.tableData = this.userData.map((item) => {
         const hrv = item.recovery
           ? Math.round(item.recovery.heartRateVariabilityRmssd * 1000)
           : 'no data'
@@ -88,15 +220,15 @@ export default {
             : 'no data'
         const remDuration =
           item.sleep && item.sleep.sleeps[0]
-            ? this.msToTime(item.sleep.sleeps[0].remSleepDuration)
+            ? this.msToMinutes(item.sleep.sleeps[0].remSleepDuration)
             : 'no data'
         const lightSleepDuration =
           item.sleep && item.sleep.sleeps[0]
-            ? this.msToTime(item.sleep.sleeps[0].lightSleepDuration)
+            ? this.msToMinutes(item.sleep.sleeps[0].lightSleepDuration)
             : 'no data'
         const swsDuration =
           item.sleep && item.sleep.sleeps[0]
-            ? this.msToTime(item.sleep.sleeps[0].slowWaveSleepDuration)
+            ? this.msToMinutes(item.sleep.sleeps[0].slowWaveSleepDuration)
             : 'no data'
 
         return {
@@ -112,9 +244,7 @@ export default {
           swsDuration: swsDuration
         }
       })
-    }
-  },
-  methods: {
+    },
     msToTime(duration) {
       let minutes = Math.floor((duration / (1000 * 60)) % 60)
       let hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
@@ -123,6 +253,9 @@ export default {
       minutes = minutes < 10 ? '0' + minutes : minutes
 
       return hours + ':' + minutes
+    },
+    msToMinutes(duration) {
+      return Math.round(duration / (1000 * 60))
     }
   }
 }
